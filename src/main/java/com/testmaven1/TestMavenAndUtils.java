ef0cd9e911150dd24042989ce2f984f7cbfb422b
Kevin <18322389273@163.com>
2018-04-18 22:50:52 +0800

    第二个测试文件

---
+++ src/main/java/com/testmaven1/TestMavenAndUtils.java
@@ -0,0 +1,17 @@
+package com.testmaven1;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateUtils;
+
+public class TestMavenAndUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Date date = new Date();
+		DateUtils.addDays(date, 1);
+
+	}
+
+}
